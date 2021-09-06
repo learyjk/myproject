b36086e899e05554d9e887e500f0cbd34fbb1686
keeganleary <leary.keegan@gmail.com>
2021-09-05 20:16:42 -0700

    new directory sturcture

--- myproject.py
+++ myproject.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, abort
+from flask import Flask, send_from_directory, abort, render_template
 import os
 application = Flask(__name__)
 
@@ -14,13 +14,19 @@ NFT_DIR = os.path.join(BASE_DIR, 'app/static/client/nft')
 application.config["NFT_FILES"] = NFT_DIR
 
 
-@application.route("/nft/<path:file_name>")
-def get_file(file_name):
+@application.route("/nft/<path:path>")
+def get_file(path):
     try:
-        return send_from_directory(directory=application.config["NFT_FILES"], path=application.config["NFT_FILES"], filename=file_name, as_attachment=False)
+        return send_from_directory(directory=application.config["NFT_FILES"], path=path, as_attachment=False)
     except FileNotFoundError:
         abort(404)
 
+
+@application.route("/nft/")
+def make_tree():
+    files = os.listdir(NFT_DIR)
+    print(files)
+    return render_template('files.html', files=files)
 
 
 if __name__ == "__main__":
